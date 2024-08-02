2740392bd77f575a1e7cd4b916e6574dec55294c
Rajkumar1208 <rajborn2win94@gmail.com>
2024-08-02 20:07:01 +0530

    create new

---
+++ src/org/vv/test/gitSample.java
@@ -0,0 +1,11 @@
+package org.vv.test;
+
+public class gitSample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to the India");
+
+	}
+
+}
